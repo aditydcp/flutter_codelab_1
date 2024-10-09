540bd21ae106b68e0ee00c66f4ad7d86a2814d0c
Aditya Cahyo <aditydcp@gmail.com>
2024-10-09 10:53:24 +0700

    update chatroom
    chat room now able to display sent messages

---
+++ lib/data/model/message.dart
@@ -0,0 +1,11 @@
+class Message {
+  final DateTime timestamp;
+  final String message;
+  final String sender;
+
+  Message({
+    required this.timestamp,
+    required this.message,
+    required this.sender,
+  });
+}
--- lib/presentation/pages/chatroom_screen.dart
+++ lib/presentation/pages/chatroom_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_codelab_1/data/model/message.dart';
 
 class ChatroomScreen extends StatefulWidget {
   const ChatroomScreen({super.key, required this.name});
@@ -12,18 +12,23 @@ class ChatroomScreen extends StatefulWidget {
 }
 
 class _ChatroomScreenState extends State<ChatroomScreen> {
-  String _message = '';
+  String _tempMessage = '';
+  final List<Message> _messages = <Message>[];
+  final TextEditingController _messagerController = TextEditingController();
 
   void setMessage(String message) {
     setState(() {
-      _message = message;
+      _tempMessage = message;
     });
   }
 
   void sendMessage() {
-    if (kDebugMode) {
-      print(_message);
-    }
+    setState(() {
+      _messages.add(Message(
+          timestamp: DateTime.now(), message: _tempMessage, sender: 'Me'));
+      _tempMessage = '';
+      _messagerController.clear();
+    });
   }
 
   @override
@@ -33,28 +38,49 @@ class _ChatroomScreenState extends State<ChatroomScreen> {
           title: Text(widget.name),
         ),
         body: SafeArea(
-            child: Column(
-          children: [
-            Text(_message),
-            const SizedBox(height: 10),
-            Row(
-              children: [
-                Expanded(
-                    child: TextField(
-                  decoration: const InputDecoration(hintText: 'Enter message'),
-                  onChanged: (value) {
-                    setMessage(value);
-                  },
-                )),
-                IconButton(
-                  icon: const Icon(Icons.send),
-                  onPressed: () {
-                    sendMessage();
-                  },
-                )
-              ],
-            )
-          ],
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: Column(
+            children: [
+              Column(
+                children: _messages.map((message) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Column(
+                      children: [
+                        Text(message.sender),
+                        Text(message.message),
+                        Text(message.timestamp.toString())
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                    controller: _messagerController,
+                    decoration:
+                        const InputDecoration(hintText: 'Enter message'),
+                    onChanged: (value) {
+                      setMessage(value);
+                    },
+                    onSubmitted: (value) {
+                      sendMessage();
+                    },
+                  )),
+                  IconButton(
+                    icon: const Icon(Icons.send),
+                    onPressed: () {
+                      sendMessage();
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
         )));
   }
 }
